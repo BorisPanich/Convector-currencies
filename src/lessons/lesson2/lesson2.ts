28071de3e944727de742e8b647649cc9474464e1
BorisPanich <panich2303@gmail.com>
2021-03-14 17:24:09 +0300

    recursion and closure in functions

--- src/lessons/lesson2/lesson2.ts
+++ src/lessons/lesson2/lesson2.ts
@@ -20,9 +20,99 @@ console.log('lesson 2');
 // https://learn.javascript.ru/recursion
 // https://www.youtube.com/watch?v=Kuq6oIN3PH0
 
+let a = 50;
+let b = 100;
+// это можно выразить в виде объекта, который далее видится
+let globalScope = {
+    a: 50,
+    b: 100,
+    f: 'Function'
+}
+a = 500; // переопределения значения а
+
+function f(arg: number) {
+    let param = 5;
+    let secondParam = 10;
+    console.log(param + secondParam);
+    const result = param + secondParam + arg;
+    arg += result;  // arg = arg + result
+    console.log(a + b); // 600
+    return () => {
+        console.log(arg + a); // 215
+        console.log(arg + b); // 315
+    };
+}
+
+f(100)();
+
+// Scope F
+let scopeF = {
+    outerScope: globalScope,
+    a: 0,
+    arg: 215,
+    param: 5,
+    secondParam: 10,
+    result: 155
+}
+____________________________
+
+let globalScope = {
+    outerScope: null,
+    a: 10,
+    f: 'Function'
+}
+let a = 10;
+let b = 50; // f: 50
+function f() {
+    console.log(b);
+}
+
+f();
+var b = 50; // ok
+let b = 50; // f: udefined
+
+
+_____________________________
+
+// Recursion
+
+function f() {
+    return f()
+}
+
+sumTo(1) = 1
+sumTo(2) = 2 + 1 = 3
+sumTo(3) = 3 + 2 + 1 = 6
+sumTo(4) = 4 + 3 + 2 + 1 = 10
+...
+sumTo(100) = 100 + 99 +
+...
++2 + 1 = 5050
+
+function sumTo(n: number) {
+    let result = 0;
+    for (let i = n; i > 0; i--) {
+        result += i;
+    }
+    return result;
+}
+
+// recursion metod
+
+function sumTo(n: number): number {
+    if (n === 1) return n;
+    return n + sumTo(n - 1);
+}
+
 
 // Task 01
 // Реализовать функцию sum которая суммирует 2 числа следующим образом sum(3)(6) === 9
+
+function sum(n: number) {
+    return function (n2: number) {
+        return n + n2;
+    }
+}
 
 // Task 02
 // Реализовать функцию makeCounter которая работает следующим образом:
@@ -33,6 +123,15 @@ console.log('lesson 2');
 // counter2(); // 1
 // counter(); // 3
 
+function makeCounter() {
+    let count = 0;
+    return function () {
+        count += 1;
+        return count;
+    }
+}
+
+
 // Task 03
 // Переписать функцию из Task 02 так, что бы она принимала число в качестве аргумента и это число было стартовым значением счетчика
 // и возвращала следующий объект методов:
@@ -40,6 +139,25 @@ console.log('lesson 2');
 // decrease: -1
 // reset: установить счетчик в 0;
 // set: установить счетчик в заданное значение;
+
+function makeCounter(startCount: number) {
+    return {
+        increase: () => ++startCount,
+        decrease() {
+            return --startCount
+        },
+        reset: () => {
+            startCount = 0;
+            return startCount;
+        },
+        set: (newCount: number) => {
+            startCount = newCount;
+            return startCount;
+        },
+        getCount: () => startCount;
+    };
+}
+
 
 // Task 04*
 // Реализовать функцию superSum которая принимает число в качестве аргумента, которое указывает на количество слагаемых
@@ -51,6 +169,24 @@ console.log('lesson 2');
 // 5) superSum(3)(2,5)(3) //10
 // 6) superSum(3)(2,5)(3,9) //10
 
+function superSum(n: number) {
+    if (n <= 0) return 0;  // без отрицательных чисел
+    if (n === 1) return (n: number) => n;
+    let _argument: number[] = [];
+
+    function helper(...args: number[]) {
+        _argument = [..._argument, ...args] // пререзаписываем 1 и сохраняем в ...arg
+        if (_argument.length >= n) {
+            _argument.length = n;
+            return _argument.reduce((acc, num) => acc + num);
+        } else {
+            return helper
+        }
+    }
+    return helper;
+}
+
+
 // P.S. типизируйте только аргументы, а при вызове функции используйте @ts-ignore
 
 // Task 05
@@ -60,4 +196,5 @@ console.log('lesson 2');
 // написать функцию, которая повторяет функционал метода flat массива на всю глубину.
 
 // just a plug
-export default () => {};
+export default () => {
+};
